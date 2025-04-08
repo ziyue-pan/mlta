740018cdddc14f94c13dd5edcc96e84c942d3417
Ziyue Pan <ziyuepan1999@gmail.com>
2025-04-08 21:17:40 +0800

    finalize original version mlta

--- src/lib/Config.h
+++ src/lib/Config.h
@@ -27,7 +27,7 @@ extern int ENABLE_MLTA;
 #define MAP_CALLER_TO_CALLEE 1
 #define UNROLL_LOOP_ONCE 1
 #define MAP_DECLARATION_FUNCTION
-#define PRINT_ICALL_TARGET
+// #define PRINT_ICALL_TARGET
 // Path to source code
 #define SOURCE_CODE_PATH "/home/kjlu/projects/kernels/linux-5.1"
 //#define PRINT_SOURCE_LINE
